0f545506934b9b390f6f2b42aab1fc61dfc0238c
AGBOHOUN Obed <136130757+Obed67@users.noreply.github.com>
2024-06-28 23:20:05 +0100

    Delete requirements..py

--- requirements..py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import Label
-from PIL import Image, ImageTk
-from customtkinter import *
-import torch
-from diffusers import StableDiffusionPipeline
-import threading
-import re
