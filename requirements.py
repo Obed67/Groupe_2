b8f394979f3b280179ee7a7daccfe9ffc5da82cf
AGBOHOUN Obed <136130757+Obed67@users.noreply.github.com>
2024-06-29 00:20:24 +0200

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import Label
+from PIL import Image, ImageTk
+from customtkinter import *
+import torch
+from diffusers import StableDiffusionPipeline
+import threading
+import re
